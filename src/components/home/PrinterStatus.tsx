77ff0be0e01f4903cc7a4fb250bffd5f6683d119
PastaLaPate <75879845+PastaLaPate@users.noreply.github.com>
2025-06-24 18:45:50 +0200

    Added attributes organizer + right side of control

--- src/components/home/PrinterStatus.tsx
+++ src/components/home/PrinterStatus.tsx
@@ -1,3 +1,4 @@
+import type { ClassValue } from "clsx";
 import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { toast } from "sonner";
@@ -11,27 +12,29 @@ import ControlledInput from "../ControlledInput";
 import HeatedPlate from "../svg/HeatedPlate";
 import Nozzle from "../svg/Nozzle";
 
-function TempViewer({
+export function TempViewer({
   octoprintState,
   target,
+  className,
+  inputClassName,
 }: {
   octoprintState: OctoprintState;
   target: PrinterTarget;
+  className?: ClassValue;
+  inputClassName?: ClassValue;
 }) {
   const [tempInput, setNumberInput] = useState("");
   const [temp, setTemp] = useState<Temp | undefined>(undefined);
 
   useEffect(() => {
-    setTemp(
-      target === "tool" ? octoprintState.toolTemp : octoprintState.bedTemp,
-    );
+    setTemp(target === "tool" ? octoprintState.toolTemp : octoprintState.bedTemp);
   }, [octoprintState]);
 
   return (
     <div
       className={cn(
-        "sm:gap-3sm:p-2 relative flex flex-1 flex-col items-center justify-between gap-2 rounded-2xl border-2 border-transparent bg-gradient-to-br from-slate-800 to-slate-900 p-2 !px-0 shadow-lg transition hover:border-blue-500 active:scale-100 sm:p-2 md:p-5",
-        "min-w-0",
+        "sm:gap-3sm:p-2 relative flex min-w-0 flex-1 flex-col items-center justify-between gap-2 rounded-2xl border-2 border-transparent bg-gradient-to-br from-slate-800 to-slate-900 p-2 !px-0 shadow-lg transition hover:border-blue-500 active:scale-100 sm:p-2 md:p-5",
+        className
       )}
       style={{
         minWidth: 0,
@@ -41,10 +44,7 @@ function TempViewer({
         {target === "tool" ? (
           <Nozzle stroke={"var(--nozzle-color)"} className="h-full w-full" />
         ) : (
-          <HeatedPlate
-            stroke={"var(--heated-bed-color)"}
-            className="h-full w-full"
-          />
+          <HeatedPlate stroke={"var(--heated-bed-color)"} className="h-full w-full" />
         )}
       </div>
       {temp && (
@@ -62,30 +62,24 @@ function TempViewer({
                 toast.success(`Set ${target}'s temp to ${Number(input)}°C`);
               })
               .catch((e) => {
-                toast.error(
-                  `Failed to set ${target}'s temp to ${Number(input)}°C: ${e.message}`,
-                );
+                toast.error(`Failed to set ${target}'s temp to ${Number(input)}°C: ${e.message}`);
               });
             setNumberInput("");
             setIsKeyboardVisible(false);
           }}
           inputClassName={cn(
             "text-center w-20 sm:w-28 sm:text-4xl xl:w-50 2xl:w-80 lg:text-5xl",
-            target === "tool"
-              ? "text-blue-200 font-bold"
-              : "text-yellow-200 font-bold",
+            target === "tool" ? "text-blue-200 font-bold" : "text-yellow-200 font-bold",
+            inputClassName
           )}
           className="flex flex-col items-center justify-center gap-2 text-center"
         />
       )}
-      <p className="text-lg text-slate-500 sm:text-xl">
-        {temp && (temp.target !== 0 ? `${temp.target}` : "")}
-      </p>
-      {/* Optional: bottom colored bar for accent */}
+      <p className="text-lg text-slate-500 sm:text-xl">{temp && (temp.target !== 0 ? `${temp.target}` : "")}</p>
       <div
         className={cn(
           "absolute bottom-0 h-2 w-full rounded-b-2xl",
-          target === "tool" ? "bg-blue-500" : "bg-yellow-500",
+          target === "tool" ? "bg-blue-500" : "bg-yellow-500"
         )}
       />
     </div>
