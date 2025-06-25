519a842aa8319ea89390c7f7c4bfab346721e786
PastaLaPate <75879845+PastaLaPate@users.noreply.github.com>
2025-06-25 14:16:52 +0200

    Better jog panel for 5" screens + better settings

--- src/routes/app/Control.tsx
+++ src/routes/app/Control.tsx
@@ -21,11 +21,11 @@ function JogPanel() {
 
   return (
     move && (
-      <div className="flex w-[50vw] items-center justify-center gap-6">
-        <div className="flex flex-col items-center justify-center gap-3">
-          <div className="flex items-center justify-center gap-4">
+      <div className="flex w-[50vw] items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-3 lg:gap-6">
+          <div className="flex items-center justify-center sm:gap-2 md:gap-3 lg:gap-4">
             <div className="relative flex flex-col items-center justify-center rounded-2xl border-2 border-transparent bg-gradient-to-br from-slate-800 to-slate-900 p-3 shadow-lg transition hover:scale-105 hover:border-blue-500 focus:outline-none active:scale-100">
-              <div className="relative sm:h-40 sm:w-40 md:h-52 md:w-52 lg:h-60 lg:w-60">
+              <div className="relative sm:h-40 sm:w-40 md:h-40 md:w-40 lg:h-60 lg:w-60">
                 <ChevronUp
                   size={50}
                   className="absolute top-0 left-1/2 -translate-x-1/2 transition hover:scale-115 active:scale-90"
@@ -55,7 +55,7 @@ function JogPanel() {
               <p className="mt-2 text-base font-bold text-blue-400">X/Y Axes</p>
             </div>
             <div className="relative flex flex-col items-center justify-center rounded-2xl border-2 border-transparent bg-gradient-to-br from-slate-800 to-slate-900 p-3 shadow-lg transition hover:scale-105 hover:border-blue-500 focus:outline-none active:scale-100">
-              <div className="relative sm:h-40 sm:w-20 md:h-52 md:w-32 lg:h-60 lg:w-40">
+              <div className="relative sm:h-40 sm:w-20 md:h-40 md:w-20 lg:h-60 lg:w-40">
                 <ChevronUp
                   size={50}
                   className="absolute top-0 left-1/2 -translate-x-1/2 transition hover:scale-115 active:scale-90"
@@ -75,25 +75,17 @@ function JogPanel() {
               <p className="mt-2 text-base font-bold text-blue-400">Z Axis</p>
             </div>
           </div>
-          <div className="relative mt-6 flex flex-col items-center justify-center rounded-2xl border-2 border-transparent bg-gradient-to-br from-slate-800 to-slate-900 p-3 shadow-lg">
+          <div className="relative flex flex-col items-center justify-center rounded-2xl border-2 border-transparent bg-gradient-to-br from-slate-800 to-slate-900 p-3 shadow-lg">
             <h2 className="mb-2 text-base font-bold text-blue-400">Step in mm</h2>
             <Tabs defaultValue="10" value={step} onValueChange={setStep}>
               <TabsList>
-                <TabsTrigger className="w-10" value="1">
-                  1
-                </TabsTrigger>
-                <TabsTrigger className="w-10" value="5">
-                  5
-                </TabsTrigger>
-                <TabsTrigger className="w-10" value="10">
-                  10
-                </TabsTrigger>
-                <TabsTrigger className="w-10" value="50">
-                  50
-                </TabsTrigger>
-                <TabsTrigger className="w-10" value="100">
-                  100
-                </TabsTrigger>
+                {["1", "5", "10", "50", "100"].map((v) => {
+                  return (
+                    <TabsTrigger className="w-10" value={v}>
+                      {v}
+                    </TabsTrigger>
+                  );
+                })}
               </TabsList>
             </Tabs>
           </div>
@@ -133,21 +125,14 @@ function GeneralControlPanel() {
             <h2 className="mb-2 text-base font-bold text-blue-400">Amount</h2>
             <Tabs defaultValue="10" value={step} onValueChange={setStep}>
               <TabsList>
-                <TabsTrigger className="w-10 md:w-7" value="1">
-                  1
-                </TabsTrigger>
-                <TabsTrigger className="w-10 md:w-7" value="5">
-                  5
-                </TabsTrigger>
-                <TabsTrigger className="w-10 md:w-7" value="10">
-                  10
-                </TabsTrigger>
-                <TabsTrigger className="w-10 md:w-7" value="50">
-                  50
-                </TabsTrigger>
-                <TabsTrigger className="w-10 md:w-7" value="100">
-                  100
-                </TabsTrigger>
+                {["1", "5", "10", "50", "100"].map((v) => {
+                  return (
+                    <TabsTrigger className="w-10 md:w-7 lg:w-10" value={v}>
+                      {" "}
+                      {v}
+                    </TabsTrigger>
+                  );
+                })}
               </TabsList>
             </Tabs>
           </div>
