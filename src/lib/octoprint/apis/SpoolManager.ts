6b4fc40741ca2593cf184fc684b825f156e1a60c
PastaLaPate <75879845+PastaLaPate@users.noreply.github.com>
2025-06-26 23:23:02 +0200

    Completed spool manager (needs select spool)

--- src/lib/octoprint/apis/SpoolManager.ts
+++ src/lib/octoprint/apis/SpoolManager.ts
@@ -3,6 +3,7 @@ import type { Axios } from "axios";
 import { OctoprintAPI } from "./OctoprintAPI";
 
 // Definitly not from OctoDash (https://github.com/UnchartedBull/OctoDash/blob/main/src/services/filament/spool-manager.octoprint.service.ts)
+// also https://github.com/UnchartedBull/OctoDash/blob/main/src/model/octoprint/plugins/spool-manager.model.ts#L1
 
 export type FilamentSpool = {
   id: number;
@@ -21,5 +22,58 @@ export default class SpoolManager extends OctoprintAPI {
   // /plugin/SpoolManager
   constructor(httpClient: Axios) {
     super(httpClient);
+  }
+
+  public async getSpools(): Promise<FilamentSpool[]> {
+    const resp = await this.httpClient.get("plugin/SpoolManager/loadSpoolsByQuery", {
+      params: {
+        filterName: "hideInactiveSpool",
+        from: 0,
+        to: 1000,
+        sortColumn: "lastUse",
+        sortOrder: "desc",
+      },
+    });
+    if (resp.status !== 200) {
+      throw new Error("Error");
+    }
+    return resp.data.allSpools.map((spool: any) => {
+      return this.parseSpool(spool);
+    });
+  }
+
+  public async getCurrentSpool(): Promise<FilamentSpool> {
+    const resp = await this.httpClient.get("plugin/SpoolManager/loadSpoolsByQuery", {
+      params: {
+        filterName: "hideInactiveSpool",
+        from: 0,
+        to: 1000,
+        sortColumn: "lastUse",
+        sortOrder: "desc",
+      },
+    });
+    if (resp.status !== 200) {
+      throw new Error("Error");
+    }
+    return resp.data.selectedSpools[0];
+  }
+
+  // TODO: Add select spool
+
+  private parseSpool(spool: any): FilamentSpool {
+    if (!spool) throw new Error("Invalid spool");
+    return {
+      color: spool.color ?? "#FF0000",
+      density: spool.density,
+      diameter: spool.diameter,
+      displayName: `${spool.vendor} - ${spool.displayName}`,
+      id: spool.databaseId,
+      material: spool.material,
+      name: spool.displayName,
+      temperatureOffset: spool.offsetTemperature ?? 0,
+      used: Number(spool.usedWeight),
+      vendor: spool.vendor,
+      weight: spool.totalWeight,
+    };
   }
 }
