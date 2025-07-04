94d0b466b0ae9ac562d0302c2e8e44ddcbdcf283
PastaLaPate <75879845+PastaLaPate@users.noreply.github.com>
2025-07-04 22:17:56 +0200

    Better translations

--- src/TranslationManager.tsx
+++ src/TranslationManager.tsx
@@ -1,11 +1,13 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import { i18nextPlugin } from "translation-check";
 import resources from "virtual:i18next-loader";
 
 i18n
   .use(LanguageDetector) // See https://github.com/i18next/i18next-browser-languageDetector
   .use(initReactI18next) // See https://www.i18next.com/overview/configuration-options
+  .use(i18nextPlugin)
   .init({
     debug: true,
     fallbackLng: "en",
