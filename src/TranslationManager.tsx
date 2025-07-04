a961a70a92ed4149b032f9bba02df4a61ccadbeb
PastaLaPate <75879845+PastaLaPate@users.noreply.github.com>
2025-07-04 14:34:30 +0200

    Base for translations

---
+++ src/TranslationManager.tsx
@@ -0,0 +1,18 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import resources from "virtual:i18next-loader";
+
+i18n
+  .use(LanguageDetector) // See https://github.com/i18next/i18next-browser-languageDetector
+  .use(initReactI18next) // See https://www.i18next.com/overview/configuration-options
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: resources,
+  });
+
+export default i18n;
